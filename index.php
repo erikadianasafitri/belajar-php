4c671250fb9a4ebb6c944716fe5bdd7ba2f4f3c1
Erika Diana Safitri <erikadian12@gmail.com>
2022-11-10 20:14:07 +0700

    initial comit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hallo Guys<br>";
+
+$nama = "Erika";
+$usia = "20";
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $usia tahun.<br>";
+
+$namaAdik = "Diana";
+$usiaAdik = "15";
+
+echo "Nama adik saya <strong>$namaAdik</strong>, usia adik saya $usiaAdik<br>";
+
+$selisihUmur = $usia - $usiaAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+
+?>
